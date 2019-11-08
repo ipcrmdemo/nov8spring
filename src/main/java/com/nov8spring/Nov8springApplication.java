42329dd8a44d73584a8d22b816ede228d7c65ac6
Matt Cadorette <mcadorette@gmail.com>
2019-11-08 15:01:36 -0500

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/nov8spring/Nov8springApplication.java
@@ -0,0 +1,12 @@
+package com.nov8spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nov8springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nov8springApplication.class, args);
+	}
+}
